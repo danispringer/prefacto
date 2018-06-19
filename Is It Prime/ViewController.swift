55dd56bce6abf9bd3ac59c90802e80f5bf6af780
Dani Springer <musicbyds@icloud.com>
2018-06-19 21:58:32 +0200

    Create new project

---
+++ Is It Prime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Is It Prime
+//
+//  Created by Dani Springer on 19/06/2018.
+//  Copyright © 2018 Dani Springer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
