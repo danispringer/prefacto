558b10daaf34807d06e1d483719b3c0904c5c105
Dani Springer <musicbyds@icloud.com>
2018-06-24 08:58:50 +0200

    feat: add sharing option

--- Prime Numbers Fun/Controller/CheckerViewController.swift
+++ Prime Numbers Fun/Controller/CheckerViewController.swift
@@ -118,6 +118,11 @@ class CheckerViewController: UIViewController, UITextFieldDelegate {
         guard number != 2 else {
             print("cannot check 2")
             let alert = Alert.shared.createAlert(alertReasonParam: Alert.alertReason.two.rawValue)
+            let shareAction = UIAlertAction(title: "Share", style: .default, handler: {
+                action in
+                self.share(number: number)
+            })
+            alert.addAction(shareAction)
             DispatchQueue.main.async {
                 self.textfield.text = ""
                 self.present(alert, animated: true)
@@ -155,11 +160,31 @@ class CheckerViewController: UIViewController, UITextFieldDelegate {
         if isPrimeBool {
             // prime
             let alert = Alert.shared.createAlert(alertReasonParam: Alert.alertReason.prime.rawValue, num: number)
+            let shareAction = UIAlertAction(title: "Share", style: .default, handler: {
+                action in
+                self.share(number: number)
+            })
+            alert.addAction(shareAction)
             DispatchQueue.main.async {
                 self.present(alert, animated: true)
                 AudioServicesPlayAlertSound(SystemSoundID(1023))
             }
         }
+    }
+    
+    func share(number: Int64) {
+        let message = "Hey, did you know that '\(number)' is a prime number? I just found out, using this app: https://itunes.apple.com/us/app/prime-numbers-fun/id1402417667 - it's really cool!"
+        
+        let activityController = UIActivityViewController(activityItems: [message], applicationActivities: nil)
+        activityController.popoverPresentationController?.sourceView = self.view // for iPads not to crash
+        activityController.completionWithItemsHandler = {
+            (activityType, completed: Bool, returnedItems: [Any]?, error: Error?) in
+            if error != nil {
+                print("Error: \(String(describing: error))")
+                print("Returned items: \(String(describing: returnedItems))")
+            }
+        }
+        present(activityController, animated: true)
     }
 }
 
