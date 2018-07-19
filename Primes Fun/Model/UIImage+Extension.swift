de63b88ed61e61faadb67b41886fc1facfc2cb70
Dani Springer <musicbyds@icloud.com>
2018-07-19 02:00:12 +0200

    fix: make tabbar completely transparent

---
+++ Primes Fun/Model/UIImage+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage+Extension.swift
+//  Primes Fun
+//
+//  Created by Dani Springer on 19/07/2018.
+//  Copyright © 2018 Dani Springer. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    static func from(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context!.setFillColor(color.cgColor)
+        context!.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!
+    }
+}
+
+// Usage:
+// let img = UIImage.from(color: .black)
