e940f05e6008c9d5a5481dd0fd7aa4acf7333fe7
Dani Springer <musicbyds@icloud.com>
2018-07-22 19:24:25 +0200

    fix: make random number copiable

--- Primes Fun/Controller/Random/RandomResultsViewController.swift
+++ Primes Fun/Controller/Random/RandomResultsViewController.swift
@@ -10,13 +10,12 @@ import Foundation
 import UIKit
 import AVFoundation
 
-class RandomResultsViewController: UIViewController {
+class RandomResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // MARK: Outlets
     
-    @IBOutlet weak var randomNumberLabel: UILabel!
+    @IBOutlet weak var randomNumberTableView: UITableView!
     @IBOutlet weak var myToolbar: UIToolbar!
-    
     
     // MARK: Properties
     
@@ -31,24 +30,11 @@ class RandomResultsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let myNumber = number else {
-            let alert = self.createAlert(alertReasonParam: alertReason.unknown.rawValue)
-            DispatchQueue.main.async {
-                AudioServicesPlayAlertSound(SystemSoundID(self.negativeSound))
-                alert.view.layoutIfNeeded()
-                self.present(alert, animated: true)
-            }
-            return
-        }
-        
-        randomNumberLabel.text = "\(myNumber)"
-        
         myToolbar.setBackgroundImage(UIImage(),
                                      forToolbarPosition: .any,
                                      barMetrics: .default)
         myToolbar.setShadowImage(UIImage(), forToolbarPosition: .any)
 
-        
     }
 
     
@@ -92,6 +78,55 @@ class RandomResultsViewController: UIViewController {
     
     @IBAction func doneButtonPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    // Delegates:
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RandomCell")!
+        
+        guard let myNumber = number else {
+            let alert = self.createAlert(alertReasonParam: alertReason.unknown.rawValue)
+            DispatchQueue.main.async {
+                AudioServicesPlayAlertSound(SystemSoundID(self.negativeSound))
+                alert.view.layoutIfNeeded()
+                self.present(alert, animated: true)
+            }
+            return cell
+        }
+        
+        cell.selectionStyle = .none
+        cell.textLabel?.textColor = UIColor(red: 0.93, green: 0.90, blue: 0.94, alpha: 1.0)
+        cell.textLabel?.font = UIFont(name: "AmericanTypewriter", size: 25)
+        
+        cell.textLabel?.text = "\(myNumber)"
+        cell.textLabel?.textAlignment = .center
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, shouldShowMenuForRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    
+    func tableView(_ tableView: UITableView, canPerformAction action: Selector, forRowAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return action == #selector(copy(_:))
+    }
+    
+    
+    func tableView(_ tableView: UITableView, performAction action: Selector, forRowAt indexPath: IndexPath, withSender sender: Any?) {
+        if action == #selector(copy(_:)) {
+            let cell = tableView.cellForRow(at: indexPath)
+            let pasteboard = UIPasteboard.general
+            pasteboard.string = cell?.textLabel?.text
+        }
     }
     
 }
