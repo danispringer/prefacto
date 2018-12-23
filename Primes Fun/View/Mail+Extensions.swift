0e41b64757e24b3e6d780c0022df05b8027b18aa
Dani Springer <musicbyds@icloud.com>
2018-12-23 06:47:11 +0100

    Create Mail+Extensions

---
+++ Primes Fun/View/Mail+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Mail+Extensions.swift
+//  Primes Fun
+//
+//  Created by Daniel Springer on 12/23/18.
+//  Copyright © 2018 Dani Springer. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+extension MFMailComposeViewController {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.tintColor = Constants.View.goldColor
+    }
+}
