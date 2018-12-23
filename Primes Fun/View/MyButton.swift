fcbe3a6afda0dfea4e33158e57e9594bb22066fb
Dani Springer <musicbyds@icloud.com>
2018-12-23 06:47:24 +0100

    fix: update colors

--- Primes Fun/View/MyButton.swift
+++ Primes Fun/View/MyButton.swift
@@ -10,10 +10,6 @@ import UIKit
 
 class MyButton: UIButton {
 
-    let dark = UIColor(red: 0.00, green: 0.16, blue: 0.21, alpha: 1.0)
-    let light = UIColor(red: 0.93, green: 0.90, blue: 0.94, alpha: 1.0)
-    let silver =  UIColor(red: 0.75, green: 0.75, blue: 0.75, alpha: 1.0)
-
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
@@ -27,8 +23,9 @@ class MyButton: UIButton {
     private func setup() {
         self.layer.cornerRadius = 7
         self.layer.masksToBounds = true
-        self.tintColor = dark
-        self.backgroundColor = light
+        self.tintColor = Constants.View.goldColor
+        self.setTitleColor(Constants.View.grayColor, for: .highlighted)
         self.contentEdgeInsets = UIEdgeInsets(top: 10.0, left: 30.0, bottom: 10.0, right: 30.0)
     }
+
 }
