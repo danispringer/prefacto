e83a666e0285cb34d1b51b775f5c1f49f46a1703
Dani Springer <musicbyds@icloud.com>
2019-01-13 04:13:19 +0100

    fix: fix notifications not updating content

--- Primes Fun/AppDelegate.swift
+++ Primes Fun/AppDelegate.swift
@@ -27,20 +27,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         didFinishLaunchingWithOptions launchOptions: [
         UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
 
+        let tab = window?.rootViewController as? UITabBarController
+        let randomVC = tab?.viewControllers?[3] as? RandomViewController
+
         // MARK: Notification
 
-        // Create notification object
         let center = UNUserNotificationCenter.current()
-
-        // Set the delegate of the Notification to be AppDelegate file
         center.delegate = self
 
-        // Create action allowing user to copy number from notification
+        center.removeAllPendingNotificationRequests()
+
+        let now = Date()
+        print("now: \(now)")
+        let calendar = Calendar.current
+        let components = DateComponents(calendar: calendar, hour: 9)
+        let next9AM = calendar.nextDate(after: now, matching: components, matchingPolicy: .nextTime)!
+
         let copyAction = UNNotificationAction(identifier: "COPY_ACTION",
                                               title: "Copy",
                                               options: UNNotificationActionOptions(rawValue: 0))
 
-        // Create category with a copy action
         let myCategory = UNNotificationCategory(identifier: "RANDOM",
                                                 actions: [copyAction],
                                                 intentIdentifiers: [],
@@ -52,56 +58,77 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         let options: UNAuthorizationOptions = [.alert, .sound]
 
         center.requestAuthorization(options: options) { (granted, error) in
-            if !granted {
+
+            guard granted else {
                 print("Something went wrong: \(String(describing: error))")
+                return
+            }
+
+            for number in 0...63 {
+                print("number: \(number)")
+                // For testing, replace .day with .second, number with 3, and next9AM with now
+                let newDate = Calendar.current.date(byAdding: .day, value: number, to: next9AM)
+                print("newDate: \(String(describing: newDate))")
+                let calendar = Calendar.current
+                let components = DateComponents(
+                    calendar: calendar,
+                    year: calendar.component(.year, from: newDate!),
+                    month: calendar.component(.month, from: newDate!),
+                    day: calendar.component(.day, from: newDate!),
+                    hour: calendar.component(.hour, from: newDate!),
+                    minute: calendar.component(.minute, from: newDate!),
+                    second: calendar.component(.second, from: newDate!))
+                print("components: \(components)")
+
+                let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+
+                let content = UNMutableNotificationContent()
+                content.title = "Your daily prime is:"
+                content.body = "\(randomVC?.makeRandomNotification() ?? 1)"
+                content.categoryIdentifier = "RANDOM-\(number)"
+
+                content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "choo.caf"))
+
+                let request = UNNotificationRequest(
+                    identifier: content.categoryIdentifier,
+                    content: content,
+                    trigger: trigger)
+                center.add(request)
             }
         }
 
         center.getNotificationSettings { (settings) in
-            if settings.authorizationStatus != .authorized {
-                // Notifications not allowed
+
+            guard settings.authorizationStatus == .authorized else {
+                print("notifications not allowed")
+                return
             }
+
         }
 
-        // Access view controller containing function that generates random prime numbers
-        let tab = window?.rootViewController as? UITabBarController
-        let randomVC = tab?.viewControllers?[3] as? RandomViewController
+        UserDefaults.standard.register(defaults: [
+            Constants.UserDef.soundEnabled: true,
+            Constants.UserDef.darkModeEnabled: true])
 
-        let content = UNMutableNotificationContent()
-        content.title = "Your daily prime is:"
+        let tabBar = UITabBar.appearance()
+        tabBar.backgroundImage = UIImage.from(color: .clear)
+        tabBar.shadowImage = UIImage.from(color: .clear)
 
-        // Set body to random prime, or 1 if returned value is nil
-        content.body = "\(randomVC?.makeRandomNotification() ?? 1)"
+        UINavigationBar.appearance().isOpaque = false
+        UINavigationBar.appearance().isTranslucent = false
 
-        content.categoryIdentifier = "RANDOM"
-        content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "choo.caf"))
+        for state: UIControl.State in [.normal, .selected, .highlighted] {
+            UIBarButtonItem.appearance().setTitleTextAttributes(
+                [
+                    NSAttributedString.Key.font: UIFont(name: Constants.Font.math, size: 25)!
+                ], for: state)
+        }
 
-        var date = DateComponents()
-        date.hour = 9
-        date.minute = 00
-        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
-
-        // For testing: comment out date and uncomment this manual trigger:
-        //let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-
-        let request = UNNotificationRequest(identifier: "RANDOM", content: content, trigger: trigger)
-
-        center.add(request)
-
-        /*
-         TODO:
-         1. clear all pending notifications
-
-         2. get current date and time
-         3. schedule 64 notifications for: current date at 9 AM, then add 1 to date
-         */
-
-        // 1
-        center.removeAllPendingNotificationRequests()
-
-        // 2
-        let currentDate = Date()
-        print(currentDate)
+        UIBarButtonItem.appearance().setTitleTextAttributes(
+            [
+                NSAttributedString.Key.font: UIFont(name: Constants.Font.math, size: 25)!,
+                NSAttributedString.Key.foregroundColor: Constants.View.grayColor
+            ], for: .disabled)
 
         return true
     }
@@ -127,5 +154,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
         completionHandler([.alert, .sound])
     }
+
+
+    // MARK: Shortcuts
+
+    func application(_ application: UIApplication,
+                     continue userActivity: NSUserActivity,
+                     restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+
+        let tab = window?.rootViewController as? UITabBarController
+        let randomVC = tab?.viewControllers?[3] as? RandomViewController
+        randomVC?.makeRandomShortcut()
+
+
+        return true
+    }
+
 
 }
