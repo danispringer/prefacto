e7fd2f4a350969b02ae6514be2276f1f4f145a91
Daniel Springer <19246409+DaniSpringer@users.noreply.github.com>
2022-10-23 00:31:51 -0400

    fixes

--- Prefacto/AppDelegate.swift
+++ Prefacto/AppDelegate.swift
@@ -30,7 +30,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
                 if CommandLine.arguments.contains("--prefactoScreenshots") {
                     // We are in testing mode, make arrangements if needed
-                    UIView.setAnimationsEnabled(false)
+                    DispatchQueue.main.async {
+                        UIView.setAnimationsEnabled(false)
+                    }
+
                 }
 
                 if let shortcutItem = launchOptions?[
